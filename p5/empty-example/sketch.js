7be1867eb386df4e828fb66a272c179514134744
Karlo Gonzales <karlo.gonzales@gmail.com>
2017-05-04 00:18:43 -0400

    Food setup

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,25 +1,39 @@
 var s;
 var speed = 1;
+var scl = 20;
+var food;
+
 function setup() {
-createCanvas(600,600);
-s = new Snake()
+    createCanvas(600, 600);
+    s = new Snake();
+    frameRate(10);
+    pickLocation();
 }
 
 function draw() {
-  background(51);
-  s.update();
-  s.show();
+    background(51);
+    s.update();
+    s.show();
+
+    fill(255,0,100);
+    rect(food.x, food.y, scl, scl);
 }
 
-
+function pickLocation() {
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
+    return food;
+}
 function keyPressed() {
-    if (keyCode === UP_ARROW){
+    if (keyCode === UP_ARROW) {
         s.dir(0, -speed);
-    }else if (keyCode === DOWN_ARROW){
-        s.dir(0,speed);
-    }else if (keyCode === RIGHT_ARROW) {
+    } else if (keyCode === DOWN_ARROW) {
+        s.dir(0, speed);
+    } else if (keyCode === RIGHT_ARROW) {
         s.dir(speed, 0);
-    }else if (keyCode === LEFT_ARROW) {
+    } else if (keyCode === LEFT_ARROW) {
         s.dir(-speed, 0);
     }
 }
--- p5/empty-example/snake.js
+++ p5/empty-example/snake.js
@@ -10,14 +10,18 @@ function Snake() {
     this.dir = function (x,y) {
         this.xspeed = x;
         this.yspeed = y;
-    }
+    };
+
     this.update = function(){
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
-    }
+        this.x = this.x + this.xspeed * scl;
+        this.y = this.y + this.yspeed * scl;
+
+        this.x = constrain(this.x, 0, width - scl);
+        this.y = constrain(this.y, 0, height - scl);
+    };
 
     this.show = function () {
         fill(255);
-        rect(this.x, this.y, 10, 10);
+        rect(this.x, this.y, scl, scl);
     }
 }
